8802d1e7dcdaa553c6b0b6c8b6cc797375f5f44c
Anubhav Singh <singhanubhav58@gmail.com>
2024-07-02 22:05:35 +0530

    fixed favicon

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,8 @@
-'use client';
 import Main from '@/Components/Main';
+export const metadata = {
+  title: 'Anubhav Singh',
+  description: 'Portfolio Website in Windows Theme',
+};
 
 export default function Page() {
   return (
